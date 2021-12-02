d6e0441598a40050b34a5f152851f8f0497483bd
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2021-12-02 09:27:22 +0530

    Complete day 16

---
+++ day16.js
@@ -0,0 +1,21 @@
+function hey(message = '') {
+  const isUpperCase = message === message.toUpperCase();
+  const isQuestion = message.endsWith('?');
+
+  if (isQuestion) {
+    if (isUpperCase) {
+      return "Calm down, I know what I'm doing!";
+    }
+    return 'Sure.';
+  }
+
+  if (isUpperCase) {
+    return 'Whoa, chill out!';
+  }
+
+  if (message.length === 0) {
+    return 'Fine. Be that way!';
+  }
+
+  return 'Whatever.';
+}
