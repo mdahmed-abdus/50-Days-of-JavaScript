48be0208b2ad19d148c17877ac7aab08d8bf8de1
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2021-12-16 09:05:00 +0530

    Complete day 35

---
+++ day35.js
@@ -0,0 +1,7 @@
+function nthlargest(arr = [], highest) {
+  arr.sort((a, b) => a - b);
+  return arr[arr.length - highest];
+}
+
+console.log(nthlargest([43, 56, 23, 89, 88, 90, 99, 652], 4)); // returns 89
+console.log(nthlargest([10, 100, 1000, 10000], 2)); // returns 1000
